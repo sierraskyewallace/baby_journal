a0f4102f783d8f0096edc78cecf7ae7ae5b1f7eb
sskwallace@gmail.com <sskwallace@gmail.com>
2021-03-06 09:46:24 -0800

    added route templates in user controller to guide

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  get '/users/:id' do 
+  end
+
+  get '/login' do 
+  end
+
+  post '/login' do 
+  end
+
+  
+  get '/register' do 
+  end
+
+  post '/signup' do 
+  end
+
+
+  get '/logout' do 
+  end
+

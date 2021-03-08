6bb5a4c22cb11f7cc5a17b52beffe67982047691
sskwallace@gmail.com <sskwallace@gmail.com>
2021-03-08 14:24:18 -0800

    created basic requests in app controller and added param validation method in update model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,66 @@
 class UsersController < ApplicationController
 
   get '/users/:id' do 
-    @users = User.find(params[:id])
-    erb :"users/show"
+    if !logged_in?
+      redirect '/index'   #may need to change to specific login page 
   end
-
+  @user = User.find(params[:id])
+  if !@user.nil? && @user == current_user
+    erb :'users/show'
+  else
+    redirect '/index' #may need to change to specific login page
+  end
+end
   get '/login' do 
+    @error_message = params[:error]
+    if !session[:user_id]
+      erb :'users/login'
+    else
+      redirect '/index'  #may need to change to specific login page
+    end
+  end
   end
 
   post '/login' do 
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/index"  # ^^
+    else
+      redirect to '/register'
+    end
+  end
   end
 
   
   get '/register' do 
-    "hello"
+    if !session[:user_id]
+      erb :'users/new'
+    else
+      redirect to "/index"  #^^
+    end
+  end
   end
 
   post '/register' do 
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/register'
+    else
+      @user = User.create(:username => params[:username], :password => params[:password])
+      session[:user_id] = @user.id
+      redirect '/index'   #^^
+    end
+  end
   end
 
 
   get '/logout' do 
+    if session[:user_id] != nil
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
   end
 end
 
--- app/models/update.rb
+++ app/models/update.rb
@@ -1,3 +1,8 @@
 class Update < ActiveRecord::Base
     belongs_to :baby
+
+    def self.valid_params?(params)
+        return !params[:name].empty? && !params[:manufacturer].empty?
+      end
+    end
 end

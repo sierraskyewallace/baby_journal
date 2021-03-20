8b719e672ea12330e8d29063aace32840ef5e906
sskwallace@gmail.com <sskwallace@gmail.com>
2021-03-20 14:24:41 -0700

    trying to get selected value from dropdown to display on next page

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -1,6 +1,6 @@
 class Baby < ActiveRecord::Base 
     belongs_to :user 
-    has_many :posts 
+    has_many :posts
     
     def self.valid_params?(params)
         return !params[:name].empty? && !params[:age].empty? && !params[:gender].empty?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password 
-    has_many :babies
+    has_many :babies 
     has_many :posts
+
     validates :username, presence: true
     validates :email, presence: true
 
